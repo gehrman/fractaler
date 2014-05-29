a453bbc00f2c9f62a5726f946cb5687d9df6e805
Geoff Ehrman <gehrman@gmail.com>
2014-05-29 08:38:19 -0400

    Starting work on STL binary support.

--- utils.py
+++ utils.py
@@ -1,11 +1,20 @@
+import struct
+
 class STLWriter:
-    def print_polytopes(polytopes, name):
+    def print_polytopes(self, polytopes, name):
         polytope_string = ""
         for polytope in polytopes:
             polytope_string += polytope.__str__()
         return "solid {}\n\n".format(name) + polytope_string\
                    + "endsolid {}\n".format(name)
-    
+
+    def unpack(self, filename):
+        triangle_unpacker = struct.Struct("3f3f3f3fH").unpack
+        triangles = []
+        with open(filename, 'rb') as file:
+            name, triangle_count = struct.unpack("80sI", file.read(struct.calcsize("80sI")))
+            for i in range(len(triangle_count))
+                triangles.append([])
 
 def test_encoding:
     cubes = []
